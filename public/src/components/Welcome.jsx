483827444312a22503f412548db599b841e9db80
NiKHiL <nikcy10.07@gmail.com>
2024-03-08 18:04:47 +0530

    first commit

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      ).username
+    );
+  }, []);
+  return (
+    <Container>
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <img src={Robot} alt="" />
+      <h3>Select a friend to load the conversation...</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: red;
+  }
+  h3{
+    padding-top: 1rem;
+    font-size: 2rem;
+  }
+`;
